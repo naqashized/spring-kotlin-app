d7629172f8948105350469229da16d837191dd9f
Tahir Naqash <tn@aazzur.com>
2024-03-23 11:39:53 +0100

    Uncommented events/EventsApplicationTests.kt

--- src/test/kotlin/com/sse/events/EventsApplicationTests.kt
+++ src/test/kotlin/com/sse/events/EventsApplicationTests.kt
@@ -1,13 +1,21 @@
-//package com.sse.events
-//
-//import org.junit.jupiter.api.Test
-//import org.springframework.boot.test.context.SpringBootTest
-//
-//@SpringBootTest
-//class EventsApplicationTests {
-//
-//	@Test
-//	fun contextLoads() {
-//	}
-//
-//}
+package com.sse.events
+
+import com.sse.events.api.EventController
+import com.sse.events.common.AbstractTestContainer
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+				properties = [ "spring.jpa.hibernate.ddl-auto=create" ]
+)
+class EventsApplicationTests: AbstractTestContainer() {
+	@Autowired
+	lateinit var eventController: EventController
+	@Test
+	fun contextLoads() {
+		assertNotNull(eventController)
+	}
+
+}
